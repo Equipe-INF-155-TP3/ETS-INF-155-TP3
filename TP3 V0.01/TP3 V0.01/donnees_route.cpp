cb75502366c2f2fa9bb86d1a91bb41634e9da64c
dupuisclarence <dupuis.clarence@gmail.com>
2015-11-20 10:14:08 -0500

    Ajout fonction distance entre 2 points et déverminage lire fichier

--- TP3 V0.01/TP3 V0.01/donnees_route.cpp
+++ TP3 V0.01/TP3 V0.01/donnees_route.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "donnees_route.h"
+#include <math.h>
 
 
 #define LONG_MAX_NOM_F 15
@@ -18,6 +19,7 @@ int lire_fichier(char *nomF, t_route *route, t_pt2d *depart, double *dir_dep, t_
 	fscanf(fichier, "%i %i\n", route->dimx, route->dimy);
 	fscanf(fichier, "%i\n", route->nb_lignes);
 	(route->liste_lignes) = (t_ligne*)malloc((route->nb_lignes)*sizeof(t_ligne));
+
 	for (i = 0; i < (route->nb_lignes); i++){
 		fscanf(fichier, "%lf %lf lf% %lf\n", route->liste_lignes[i].ptA.X);
 	}
@@ -26,9 +28,21 @@ int lire_fichier(char *nomF, t_route *route, t_pt2d *depart, double *dir_dep, t_
 	*dir_dep = dir_dep_deg*RAD_A_DEG; // Conversion des radians en degrés
 
 	fscanf(fichier, "%i\n", chemin->nb_pts);
-	(chemin->liste_pts) = (t_chemin*)malloc((chemin->nb_pts)*sizeof(t_chemin)); //erreur ...
+	(chemin->liste_pts) = (t_pt2d*)malloc((chemin->nb_pts)*sizeof(t_pt2d)); //erreur ...
 	for (i = 0; i < (chemin->nb_pts); i++){
 		fscanf(fichier, "%lf %lf\n", chemin->liste_pts[i].X, chemin->liste_pts[i].Y);
 	}
 
 }
+
+
+double dist(t_pt2d p1, t_pt2d p2){
+
+	double dx, dy; // Déclaration des variables
+
+	dx = (p2.X) - (p1.X); // Différence entre 
+	dy = (p2.Y) - (p1.Y);
+	
+	return sqrt(dx*dx+dy*dy);
+}
+

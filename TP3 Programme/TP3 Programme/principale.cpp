b0c3c64defee637843e5e4d74bda8c5ccdd047d9
reggoal <villeneuveregis@gmail.com>
2015-11-27 11:56:59 -0500

    main

--- TP3 Programme/TP3 Programme/principale.cpp
+++ TP3 Programme/TP3 Programme/principale.cpp
@@ -8,13 +8,28 @@
 
 int main()
 {
+	char *nomF;
+	t_route route;
+	t_chemin chemin;
+	t_pt2d depart;
+	double dir_depart;
+	int nb_obs;
+
+	initialiser_graphique();
+
+	nomF = saisie_nomF();
+
+	if (lire_fichier(nomF, &route, &depart, &dir_depart, &chemin, &nb_obs))
+	{
+		effacer_ecran();
+
+		dessiner_route(&route);
+		//dessiner_chemin();
 	int nomF;
 
 	initialiser_graphique();
 
 	saisie_nomF();
-	
-
 	
 
 	effacer_ecran();
@@ -23,6 +38,8 @@ int main()
 
 	pause_ecran();
 
+		pause_ecran();
+	}
 
 	system("pause");
 	return EXIT_SUCCESS;

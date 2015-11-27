a5bde75044afc28f93c33f6e420993602dd1d153
reggoal <villeneuveregis@gmail.com>
2015-11-27 10:01:49 -0500

    Remplacer main

---
+++ TP3 Programme/TP3 Programme/principale.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "winBGIm.h"    
+#include "donnees_route.h"
+#include "graphiques_lib.h"
+#include "t_auto.h"
+
+int main()
+{
+	int nomF;
+
+	initialiser_graphique();
+
+	saisie_nomF();
+	
+
+	
+
+	effacer_ecran();
+	dessiner_route();
+	//dessiner_chemin();
+
+	pause_ecran();
+
+
+	system("pause");
+	return EXIT_SUCCESS;
+}

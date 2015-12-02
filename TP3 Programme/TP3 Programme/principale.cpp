1237cc1893e0737f4c262719b3c4109ddfbaf0f4
Philippe117 <Philippe117@live.fr>
2015-12-02 13:32:29 -0500

    Ajout de l'en-tête du programe

--- TP3 Programme/TP3 Programme/principale.cpp
+++ TP3 Programme/TP3 Programme/principale.cpp
@@ -1,3 +1,74 @@
+  //********************##############################************************/
+ //*********************#  Présentation du programe  #***********************/
+//**********************##############################**********************/
+/*
+    Titre: La voiture intelligente autoguidée
+
+	Auteurs:
+		- Philippe La Madeleine -LAMP21099204
+		- //////////////  écriver votre nom et votre code permanent ici  //////////////
+		- //////////////  écriver votre nom et votre code permanent ici  //////////////
+
+	Cours: INF-155
+
+	Groupe: 2
+
+	Objectif:	Programer une voiture qui suivras un parcour prédéfini a 
+				travest un environement simulé en évitant les colision avec
+				des obstacles dynamiques.
+				
+
+	Liste des fonctions rédigées:
+
+		* Module principale
+			- main
+
+		* Module donnees_route
+			- detruire_chemin
+			- dist
+			- lire_fichier
+
+		* Module graphique
+			- afficher_menu
+			- afficher_pos
+			- delai
+			- dessiner_auto
+			- dessiner_chemin
+			- dessiner_ligne
+			- dessiner_rond
+			- dessiner_route
+			- detecter_pixel
+			- effacer_ecran
+			- effacer_route
+			- fermer_graphique
+			- initialiser_graphique
+			- pause_ecran
+			- pixel
+			- saisie_nom
+			- saisie_touche
+
+		* Module t_auto
+			- changer_acc_auto
+			- deplacer_auto
+			- init_auto
+			- obt_pos_auto
+		
+		* Module t_obstacle
+			- afficher_obs
+			- cree_obs
+			- deplacer_obs
+			- detecter_auto
+			- detruire_obs
+
+		* Module WINBGIM
+
+		
+	Date de création: 6 novembre 2015
+
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,6 +77,33 @@
 #include "graphiques_lib.h"
 #include "t_auto.h"
 
+  //***************########################################*******************/
+ //****************#  Définition des fonctions statiques  #******************/
+//*****************########################################*****************/
+
+
+  //********************#############################************************/
+ //*********************#  Fonction: choix du menu  #***********************/
+//**********************#############################**********************/
+static char choix_menu( const char *choix_possible){
+	char choix = 0, i;
+	do {
+		choix=toupper(getch()); // Percoit la première touche appuillé en majuscule.
+		// Compare la touche appuillé avec tout les élément de la chaine de
+		// caractère "choix_possible".
+		for(i=0;choix_possible[i];i++){
+			if (choix_possible[i] == choix)
+				return choix; // Renvoit la touche appuillé si celle ci est dans la liste.
+		}
+	} while (choix);//Recommence tant que la touche ne correspond pas.
+	return choix;
+}
+
+
+
+  //******************##################################**********************/
+ //*******************#  Fonction: Entrée du programe  #*********************/
+//********************##################################********************/
 int main()
 {
 	char *nomF;
